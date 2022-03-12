0cad3cb74646948466926c50c3b0dedcfc3ac84a
Pedro Teixeira <pedro.novo.93@gmail.com>
2022-03-12 18:08:35 +0000

    small fix

--- components/posts/single-post-page/IndividualPostComment.tsx
+++ components/posts/single-post-page/IndividualPostComment.tsx
@@ -2,13 +2,18 @@ import React, { useState } from "react";
 import { Box, Button, Typography } from "@mui/material";
 import { IComment } from "../../../src/types";
 import NestedCommentBox from "./NestedCommentBox";
+import { useCommentContext } from "../../../components/context/CommentsContext";
+import BeenhereIcon from "@mui/icons-material/Beenhere";
 
 interface IEachPostComment {
    comment: IComment;
 }
 
 const IndividualPostComment = ({ comment }: IEachPostComment) => {
+   const { nestedComments } = useCommentContext();
    const [displayCommentBox, displayCommentBoxSet] = useState(false);
+   const [displaySecondCommentBox, displaySecondCommentBoxSet] =
+      useState(false);
 
    return (
       <Box>
@@ -19,9 +24,36 @@ const IndividualPostComment = ({ comment }: IEachPostComment) => {
             variant='text'
             onClick={() => displayCommentBoxSet(!displayCommentBox)}
          >
-            Comment
+            {displayCommentBox ? `Close` : `Comment`}
          </Button>
          {displayCommentBox && <NestedCommentBox parentID={comment.id} />}
+         {nestedComments?.map((nestedComment) => {
+            if (nestedComment.parentID === comment.id) {
+               return (
+                  <Box
+                     key={comment.id}
+                     marginTop={2}
+                     padding={3}
+                     borderRadius={2}
+                     sx={{ background: "#FFFFFF" }}
+                  >
+                     {nestedComment?.text}
+                     <Button
+                        variant='text'
+                        onClick={() =>
+                           displaySecondCommentBoxSet(!displaySecondCommentBox)
+                        }
+                        sx={{ display: "block" }}
+                     >
+                        {displaySecondCommentBox ? `Close` : `Answer`}
+                     </Button>
+                     {displaySecondCommentBox && (
+                        <NestedCommentBox parentID={nestedComment.id} />
+                     )}
+                  </Box>
+               );
+            }
+         })}
       </Box>
    );
 };
